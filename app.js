5812918a940b93bcf4ff2b6bacd1f89c7567c072
Jae-hoDo <doje416@gmail.com>
2024-10-05 23:07:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const a = 5;
-const b = 2;
-let myName = "jaeho";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "do";
-
-console.log("your name is " + myName);
+const amIFat = false;
